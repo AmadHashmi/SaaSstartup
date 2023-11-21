aff71b41eaa5ec61e74712a35a15f268d0f08f88
Amad Ul Hassan <48374522+AmadHashmi@users.noreply.github.com>
2023-11-21 01:08:23 +0100

    implemented user module
    implemented user module
    -controller
    -service
    -entity (for db setup)
    -interface (model)
    and the api end points

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
+  @Get(':id')
+  findOne(@Param() params): Observable<User> {
+    return this.userService.findOne(params.id);
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+    return this.userService.updateOne(Number(id), user);
+  }
+}
