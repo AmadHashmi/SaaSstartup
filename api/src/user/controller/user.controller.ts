fb764147ce9320b4f40384c19793cbb8fb238177
Amad Ul Hassan <48374522+AmadHashmi@users.noreply.github.com>
2023-11-22 23:56:55 +0100

    finished login end point and jwt
    finished login end point and jwt
    controller method + service method

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,18 +1,35 @@
 import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
 import { UserService } from '../service/user.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { User } from '../models/user.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
   @Post()
-  create(@Body() user: User): Observable<User> {
-    return this.userService.create(user);
+  create(@Body() user: User): Observable<User | any> {
+    return this.userService.create(user).pipe(
+      map((user: User) => user),
+      catchError((err) => of({ error: err.message })),
+    );
+  }
+
+  @Post('login')
+  login(@Body() user: User): Observable<object> {
+    return this.userService.login(user).pipe(
+      map((jwt: string) => {
+        return { access_token: jwt };
+      }),
+    );
   }
   @Get(':id')
   findOne(@Param() params): Observable<User> {
     return this.userService.findOne(params.id);
+  }
+
+  @Get()
+  findAll(): Observable<any> {
+    return this.userService.findAll();
   }
 
   @Put(':id')
--- api/src/user/service/user.service.ts
+++ api/src/user/service/user.service.ts
@@ -1,26 +1,91 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../models/user.entity';
-import { Observable, from } from 'rxjs';
+import { Observable, catchError, from, map, switchMap, throwError } from 'rxjs';
 import { User } from '../models/user.interface';
+import { AuthService } from 'src/auth/services/auth.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private authService: AuthService,
   ) {}
 
   create(user: User): Observable<User> {
-    return from(this.userRepository.save(user));
+    return this.authService.hashPassword(user.password).pipe(
+      switchMap((passwordHash: string) => {
+        const newUser = new UserEntity();
+        newUser.name = user.name;
+        newUser.username = user.username;
+        newUser.email = user.email;
+        newUser.password = passwordHash;
+
+        return from(this.userRepository.save(newUser)).pipe(
+          map((user: User) => {
+            const { password, ...result } = user;
+            return result;
+          }),
+          catchError((err) => throwError(err)),
+        );
+      }),
+    );
   }
 
   findOne(id: number): Observable<User> {
-    return from(this.userRepository.findOne({ where: { id: id } }));
+    return from(this.userRepository.findOne({ where: { id: id } })).pipe(
+      map((user: User) => {
+        const { password, ...result } = user;
+        return result;
+      }),
+    );
+  }
+
+  findAll(): Observable<any> {
+    return from(this.userRepository.find());
   }
 
   updateOne(id: number, user: User): Observable<any> {
+    delete user.email;
+    delete user.password;
     return from(this.userRepository.update(id, user));
+  }
+
+  login(user: User): Observable<string> {
+    return this.validateUser(user.email, user.password).pipe(
+      switchMap((user: User) => {
+        if (user) {
+          return this.authService
+            .generateJWT(user)
+            .pipe(map((jwt: string) => jwt));
+        } else {
+          return 'Wrong credentials';
+        }
+      }),
+    );
+  }
+
+  validateUser(email: string, password: string): Observable<User> {
+    return this.findByMail(email).pipe(
+      switchMap((user: User) =>
+        this.authService.comparePassword(password, user.password).pipe(
+          map((match: boolean) => {
+            if (match) {
+              const { password, ...result } = user;
+              return result;
+            } else {
+              throw Error;
+            }
+          }),
+        ),
+      ),
+    );
+  }
+
+  findByMail(email: string) {
+    return from(this.userRepository.findOne({ where: { email: email } }));
   }
 }
